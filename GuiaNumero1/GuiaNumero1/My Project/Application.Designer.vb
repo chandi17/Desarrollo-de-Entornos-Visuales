57799390166837a9302038f7f018769ace65418c
Elmer Chandia <65907732+chandi17@users.noreply.github.com>
2020-06-06 19:28:52 -0600

    Actializació de los forms de tarea

--- GuiaNumero1/GuiaNumero1/My Project/Application.Designer.vb
+++ GuiaNumero1/GuiaNumero1/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.GuiaNumero1.Calculador
+            Me.MainForm = Global.GuiaNumero1.FormulaCuadratica
         End Sub
     End Class
 End Namespace
